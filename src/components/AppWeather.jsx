8cd24fb4050c533df6c9e0a751a17853502300b7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-26 19:14:00 -0600

    catch error functionality

--- src/components/AppWeather.jsx
+++ src/components/AppWeather.jsx
@@ -5,14 +5,15 @@ import Weather from "./Weather";
 
 export default function AppWeather() {
 
-  const { weather, loading } = useWeather();
+  const { weather, loading, error } = useWeather();
 
   return (
     <>
       <main className="two-columns">
         <Form />
 
-        {loading ? <Spinner /> :
+        { loading ? <Spinner /> :
+          error ? <p>Sin resultados.</p> :
           (Object.keys(weather).length) ? <Weather /> : <p>Selecciona tu país y ciudad para consultar el clima.</p>}
       </main>
     </>
--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -12,6 +12,7 @@ export const WeatherProvider = ({ children }) => {
 
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const createSearch = (e) => {
     setSearch({
@@ -22,15 +23,18 @@ export const WeatherProvider = ({ children }) => {
 
   const getWeather = async data => {
     setLoading(true);
+    setError(false);
     const { country, city } = data;
     const appID = import.meta.env.VITE_API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appID}`;
     try {
       const { data } = await axios(url);
       setWeather(data);
-      setLoading(false);
     } catch (error) {
+      setError(true);
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -42,6 +46,7 @@ export const WeatherProvider = ({ children }) => {
         getWeather,
         weather,
         loading,
+        error,
       }}
     >
       {children}
